6c9d616675ec4aa212fd0562df9c7ad8578dc1f8
linsixu <linsixu@ruqimobility.com>
2025-10-28 11:35:31 +0800

    删除多余逻辑

--- composeApp/src/iosMain/kotlin/com/music/classroom/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/music/classroom/MainViewController.kt
@@ -6,18 +6,7 @@ import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIStatusBarStyle
 import platform.UIKit.UIStatusBarStyleDarkContent
 
-fun MainViewController() = ComposeUIViewController(
-    // 配置状态栏文字样式为黑色（核心1）
-    configure = {
-        delegate = object: ComposeUIViewControllerDelegate {
-            override val preferredStatusBarStyle: UIStatusBarStyle?
-                get() = UIStatusBarStyleDarkContent
-
-            override val prefersStatusBarHidden: Boolean?
-                get() = false
-        }
-    }
-) {
+fun MainViewController() = ComposeUIViewController{
     // 你的 Compose 根组件
     App()
 }
